9118f18bf20bdebbba49ded57427fa05cb77af0a
LCedric <Leroy_Cedric@outlook.com>
2023-04-23 16:06:43 +0200

    Model added, base CSS with materialize, and first directive added'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  templateUrl: 'app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'pokemon';
+export class AppComponent implements OnInit {
+  pokemons: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  seletPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemons.find(
+      p => p.id?.toString() === pokemonId
+    );
+
+    this.pokemonSelected = pokemon;
+  }
+
+  ngOnInit() {
+    console.table(this.pokemons);
+  }
 }
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBorderCard]',
+})
+export class BorderCardDirective {
+  private initialColor = '#f5f5f5';
+  private defaultColor = '#009688';
+  private defaultHeight = 180;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
+  }
+
+  @Input('appBorderCard') borderColor: string | undefined;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.borderColor || this.defaultColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
+}
